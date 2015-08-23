ddb90209bac28c6ca56ba7ed1559b3fcf49adc00
Paris Jordan <parisjordanofficial@gmail.com>
2015-08-22 21:42:03 -0500

    instagram clone for ms

---
+++ ParseStarterProject/cell.swift
@@ -0,0 +1,19 @@
+//
+//  cell.swift
+//  ParseStarterProject
+//
+//  Created by Paris Jordan 08/18/2018.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+    
+    @IBOutlet var postedImage: UIImageView!
+    
+    @IBOutlet var username: UILabel!
+    
+    @IBOutlet var message: UILabel!
+    
+}
